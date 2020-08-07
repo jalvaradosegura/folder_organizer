acfca397ebfb37efe299dd7065cf9e8cdd03dba5
Jorge Alvarado <alvaradosegurajorge@gmail.com>
2020-08-06 23:50:18 -0400

    Add an assert to the set_environment_variables function

--- utils/environment_variables.py
+++ utils/environment_variables.py
@@ -2,6 +2,8 @@ import os
 
 
 def set_environment_variables(environment_file='.env'):
+    assert os.path.isfile(environment_file), 'File for environment variables' \
+                                             ' does not exist'
     with open(environment_file) as f:
         for var in f.readlines():
             if '=' in var and not var.startswith('#'):
